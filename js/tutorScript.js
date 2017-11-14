019f6baec9d37cfe17e93e542e7f3caf3191cb59
OKelt <oakelton@gmail.com>
2017-11-13 19:07:26 -0800

    Updated, refactored Find Tutor page
    Put script for find_tutor.html in "./js/tutorScript.js". Refactored script. Changed "class_style.css"

--- js/tutorScript.js
+++ js/tutorScript.js
@@ -16,6 +16,70 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");//toggle whether to show dropdown menu
 }//end myFunction
 
+/**
+ * inserts a list of appt times into a given element
+ * @param element: the element to insert the list into. Does not need to be
+ * enclosed in $()
+ * @param appts: 2d array containing appt times in form [val, txt]. val is 24-hr time,
+ * txt is the 12-hr time to display
+ * @return none
+ */
+function insertApptTimes(element, appts) {
+	if (appts == null || appts.length < 1) return;
+	element = $(element);
+	for (var i = 0; i < appts.length; i++) {
+		if (appts[i].length != 2) return;//edge case
+		var val = appts[i][0]; var txt = appts[i][1];
+		var option = '<option value="' + val + '">' + txt + '</option>';
+		//console.log(option);
+		element.append(option);
+	}//end for loop i
+}//end insertApptTimes
+/**
+ * fills "make appointment" menu with possible times for appointment. Values in 24 hr time,
+ * text in 12 hr time + am/pm.
+ * @param firstTime: first available time; string in format "hh:mm". 24 hr time
+ * @param lastTime: last available time; string in format "hh:mm". 24 hr time
+ * @param interval: number of minutes between each available appointment. int
+ * @return 2d array containing arrays of 24-hr values and 12-hr times [val, txt]
+ */
+function createOptions(firstTime, lastTime, interval) {
+	var firstInfo = firstTime.split(':');//splits firstTime into ["hour", "minutes"]
+	var lastInfo = lastTime.split(':');//splits lastTime into ["hour", "minutes"]
+	var firstNums = [parseInt(firstInfo[0],10), parseInt(firstInfo[1],10)];//firstInfo to ints
+	var lastNums = [parseInt(lastInfo[0],10), parseInt(lastInfo[1],10)];//firstInfo to ints
+	
+	var apptTimes = [];//values and text for appointment times, each in an array of length 2
+	var i = 0, safety = 999999;//iterators
+	var hr = firstNums[0], mins = firstNums[1];//initial hours and minutes
+	var limit = (lastNums[0] * 60) + lastNums[1]; var time = (hr * 60) + mins;//total time in minutes
+	
+	/*Iterate through all possible appt times at interval between firstTime and lastTime,
+	 create new array holding them.*/
+	while (time <= limit && i < safety) {
+		//txt = text between tags, val = value attribute
+		var txt = "", val = "", AmPm = ' am';
+		if (mins < 10) val += hr + ":0" + mins;//ex: "10:06" instead of "10:6"
+		else val = "" + hr + ":" + mins;
+		var hrDisp = hr;
+		if (hrDisp >= 12) {//sets time in am/pm
+			hrDisp = hr % 12;
+			if (hrDisp == 0) hrDisp = 12;
+			AmPm = ' pm';
+		}
+		if (mins < 10) txt += hrDisp + ":0" + mins + AmPm;//ex: "10:06" instead of "10:6"
+		else txt += hrDisp + ":" + mins + AmPm;
+		mins += interval;
+		while (mins >= 60) {
+			mins -= 60;
+			hr++;
+		}
+		apptTimes.push([val, txt]);
+		i++; time = (hr * 60) + mins;//time in minutes
+	}//end while loop
+	return apptTimes;
+}//end function createOptions
+
 //Close the dropdown if the user clicks outside of it
 function closeDropdown(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -30,42 +94,18 @@ function closeDropdown(event) {
     }
   }
 }//end closeDropdown
-//console.log(tutors);
-
-var block = '<div class="row">\n';//new row
-var infoblock = '<div class="info row_block">\n';//new info block
-var btn_block = '<div class="btn_block">\n';//new button block
 
 /* loads list of tutors. List of tutors contained in tutors.js
  * TODO: move tutor data to Database.js*/
 function loadtutors() {
-	var node = document.getElementsByClassName('container')[0];
-	var html = '';
-	for(var i in tutors){
-		var div = document.createElement('div');
-  		
-		var tutor = tutors[i];
-		var name = tutor['name'];
-		var age = tutor['age'];
-		var rate = tutor['rating'];
-		var subject = tutor['subject'];
-		var pic = tutor['pic'];
-
-		html += block + '<img class="row_block" '+ 'src="./'+pic+'">'+'\n';
-		html = html + infoblock + 'name:  '+name+'<br>\n';
-		html = html + 'age:  '+age + '<br>\n' + 'rating:  '+rate+'<br>\n';
-		html = html + 'subject:  '+subject+'<br>\n';
-		html = html + '</div>\n' + btn_block + '<button class="appt_btn" onclick="';
-		html = html + 'appoint(\'' + name + '\')">make<br>appointment</button>\n';
-		html = html + '</div>\n</div>\n';
-	}
-	console.log(html);
-	div.innerHTML = html;
-
-	while(div.children.length > 0){
-		node.appendChild(div.children[0]);
-	}
+	var node = document.getElementById('tutor-table');
+	var source = document.getElementById('template-1').innerHTML;
+	var template = Handlebars.compile(source);
 	
+	for (var i in tutors) {
+		var currHTML = template(tutors[i]);
+		node.innerHTML += currHTML;
+	}
 }//end loadtutors
 
 /************************Begin Main Method**********************/
@@ -73,6 +113,9 @@ var main = function() {
 	loadtutors();
 	console.log(tutors);
 	window.addEventListener('click',closeDropdown);//close dropdown when user clicks outside menu
+	/*Create appointment times at 15 minute intervals from 9:00 am to 8:00 pm*/
+	var appointments = createOptions('9:00', '20:00', 15);
+	insertApptTimes('#time', appointments);
 };//end main method
 
 window.onload = main;//executes main function on load
--- js/tutors.js
+++ js/tutors.js
@@ -1,30 +1,30 @@
-var tutors = {0:{'name':'Andrew',
+var tutors = {0:{'name':'Andrew Wu',
 				'age':'21',
 				'rating':'4.0',
-				'pic':'./ppap.jpg',
+				'pic':'http://www.freegreatpicture.com/files/94/28287-business-people-stock.jpg',
 				'subject':'AP Calculus'},
 
-			 1:{'name':'Jack',
+			 1:{'name':'Jack Daniels',
 				'age':'20',
 				'rating':'3.7',
-				'pic':'./ppap.jpg',
+				'pic':'http://compforce.typepad.com/.a/6a00d83451df4569e201a73e1384da970d-pi',
 				'subject':'AP Comp Sci'},
 
-			 2:{'name':'Alex',
+			 2:{'name':'Alex Liang',
 				'age':'18',
 				'rating':'4.5',
-				'pic':'./ppap.jpg',
+				'pic':'https://pbs.twimg.com/media/CynmmdYWgAAjky1.jpg',
 				'subject':'AP Physics'},
-			3:{'name':'Jim',
+			3:{'name':'Jim Beam',
 				'age':'18',
 				'rating':'4.5',
-				'pic':'./ppap.jpg',
+				'pic':'https://thumb1.shutterstock.com/display_pic_with_logo/238468/141327337/stock-photo-happy-man-giving-okay-sign-portrait-on-white-background-141327337.jpg',
 				'subject':'AP Physics'},
-			4:{'name':'Garry',
+			4:{'name':'Gary Johnson',
 				'age':'18',
 				'rating':'4.5',
-				'pic':'./ppap.jpg',
-				'subject':'Math'},
+				'pic':'https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Gary_Johnson_campaign_portrait.jpg/1200px-Gary_Johnson_campaign_portrait.jpg',
+				'subject':'Math'}
 
 			};
 
@@ -34,15 +34,13 @@ function appoint(name){
 	modal.style.display = "block";
 	document.getElementById('name').innerHTML = name;
 	span.onclick = function() {
-    modal.style.display = "none";
+    	modal.style.display = "none";
 	}
 	window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+    	if (event.target == modal) modal.style.display = "none";
+	}
 
-}
+}//end function appoint
 
 function cancel(){
 	var modal = document.getElementById('pop');
